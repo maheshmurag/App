6c83c237f5ffbe3d392f4484d7ff0a8608e5de3b
Mahesh Murag <maheshmmurag@gmail.com>
2015-08-17 16:04:44 -0700

    optimized search & shifted .data

--- MVHSApp/www/js/controllers.js
+++ MVHSApp/www/js/controllers.js
@@ -3,8 +3,8 @@ angular.module('starter.controllers', ['ionic', 'ngMaterial', 'ion-alpha-scroll'
 .controller('DashCtrl', function ($scope) {})
 
 .controller('ChatsCtrl', function ($scope, Chats, $ionicTabsDelegate) {
-        //$scope.$on('$ionicView.enter', function(e) {
-        //});
+//        $scope.$on('$ionicView.enter', function(e) {
+//        });
 
         $scope.chats = Chats.all();
         $scope.remove = function (chat) {
@@ -58,48 +58,12 @@ angular.module('starter.controllers', ['ionic', 'ngMaterial', 'ion-alpha-scroll'
             previous = selected;
             selected = tabs[current];
         });
-
-
-
-
-
     })
     .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
         $scope.chat = Chats.get($stateParams.chatId);
     })
 
 .controller('AccountCtrl', function ($scope) {
-    $scope.settings = {
-        enableFriends: true
-    };
-
-
-    $scope.searchText = "";
-    $scope.searchQuery = [];
-
-    $scope.queryMatches = function (query) {
-        var arr = [];
-        for (var i = 0; i < $scope.data.results.length; i++) {
-            var fname = $scope.data.results[i].firstName;
-            var lname = $scope.data.results[i].lastName;
-            if (fname.toLowerCase().indexOf(("" + query).toLowerCase()) == 0 ||
-                lname.toLowerCase().indexOf(("" + query).toLowerCase()) == 0)
-                arr.push($scope.data.results[i].name + "");
-        }
-        $scope.searchQuery = arr;
-    };
-    $scope.showDivider = function (index) {
-        //        console.log("called:"+index)
-        if (index == 0) return true;
-        if (index != $scope.data.results.length - 1 && index > 0) {
-
-            if ($scope.data.results[index].lastName.charAt(0) != $scope.data.results[index - 1].lastName.charAt(0)) {
-                //                console.log("returning true for index " + index);
-                return true;
-            }
-
-        }
-    }
     $scope.data = {
         "offset": 0,
         "title": "Monta Vista High School: All Staff List",
@@ -1833,4 +1797,42 @@ angular.module('starter.controllers', ['ionic', 'ngMaterial', 'ion-alpha-scroll'
             }
         ]
     };
+    $scope.settings = {
+        enableFriends: true
+    };
+    $scope.$on('$ionicView.enter', function(e) {
+        $scope.filteredResults = $scope.data.results;
+    });
+
+    $scope.searchText = "";
+    $scope.searchQuery = [];
+
+    $scope.lastQuery = "";
+    $scope.queryMatches = function (query) {
+        if($scope.lastQuery != "")
+            if(query.length < $scope.lastQuery.length)
+                $scope.filteredResults = $scope.data.results;
+        var arr = [];
+        var tmp = [];
+        for (var i = 0; i < $scope.filteredResults.length; i++) {
+            var fname = $scope.filteredResults[i].firstName;
+            var lname = $scope.filteredResults[i].lastName;
+            if (fname.toLowerCase().indexOf(("" + query).toLowerCase()) == 0 ||
+                lname.toLowerCase().indexOf(("" + query).toLowerCase()) == 0){
+                arr.push($scope.filteredResults[i].name + "");
+                tmp.push($scope.filteredResults[i])
+            }
+        }
+        $scope.lastQuery = query;
+        $scope.filteredResults = tmp;
+        console.log("Saagar: " + $scope.filteredResults.length)
+        $scope.searchQuery = arr;
+    };
+    $scope.showDivider = function (index) {
+        if (index == 0) return true;
+        if (index != $scope.data.results.length - 1 && index > 0)
+            if ($scope.data.results[index].lastName.charAt(0) != $scope.data.results[index - 1].lastName.charAt(0))
+                return true;
+    }
+
 });
