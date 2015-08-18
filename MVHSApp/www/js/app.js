643ffaad821784b01084015199f3f3c4c6277cbf
Mahesh Murag <maheshmmurag@gmail.com>
2015-08-17 17:30:55 -0700

    formatted all code

--- MVHSApp/www/js/app.js
+++ MVHSApp/www/js/app.js
@@ -5,81 +5,81 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngMaterial', 'ion-alpha-scroll'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngMaterial', 'ion-alpha-scroll'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+.run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
+        }
+        if (window.StatusBar) {
+            // org.apache.cordova.statusbar required
+            StatusBar.styleLightContent();
+        }
+    });
 })
 
-.config(function($stateProvider, $urlRouterProvider, $mdThemingProvider) {
+.config(function ($stateProvider, $urlRouterProvider, $mdThemingProvider) {
     $mdThemingProvider.theme('default').primaryPalette('purple').accentPalette('indigo');
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.schedule', {
-      url: '/schedule',
-      views: {
-          'tab-schedule': {
-              templateUrl: 'templates/tab-schedule.html',
-              controller: 'ScheduleCtrl'
-        }
-      }
-    })
-    .state('tab.teacher-detail', {
-      url: '/teachers/:teacherId',
-      views: {
-        'tab-teachers': {
-          templateUrl: 'templates/teacher-detail.html',
-          controller: 'TeacherDetailCtrl'
-        }
-      }
+    // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
     })
 
-  .state('tab.teachers', {
-    url: '/teachers',
-    views: {
-      'tab-teachers': {
-        templateUrl: 'templates/tab-teachers.html',
-        controller: 'TeacherListCtrl'
-      }
-    }
-  });
+    // Each tab has its own nav history stack:
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    .state('tab.dash', {
+        url: '/dash',
+        views: {
+            'tab-dash': {
+                templateUrl: 'templates/tab-dash.html',
+                controller: 'DashCtrl'
+            }
+        }
+    })
+
+    .state('tab.schedule', {
+            url: '/schedule',
+            views: {
+                'tab-schedule': {
+                    templateUrl: 'templates/tab-schedule.html',
+                    controller: 'ScheduleCtrl'
+                }
+            }
+        })
+        .state('tab.teacher-detail', {
+            url: '/teachers/:teacherId',
+            views: {
+                'tab-teachers': {
+                    templateUrl: 'templates/teacher-detail.html',
+                    controller: 'TeacherDetailCtrl'
+                }
+            }
+        })
+
+    .state('tab.teachers', {
+        url: '/teachers',
+        views: {
+            'tab-teachers': {
+                templateUrl: 'templates/tab-teachers.html',
+                controller: 'TeacherListCtrl'
+            }
+        }
+    });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/dash');
 
 });
--- MVHSApp/www/js/controllers.js
+++ MVHSApp/www/js/controllers.js
@@ -2,9 +2,9 @@ angular.module('starter.controllers', ['ionic', 'ngMaterial', 'ion-alpha-scroll'
 
 .controller('DashCtrl', function ($scope) {})
 
-    .controller('ScheduleCtrl', function ($scope, Chats, $ionicTabsDelegate) {
-//        $scope.$on('$ionicView.enter', function(e) {
-//        });
+.controller('ScheduleCtrl', function ($scope, Chats, $ionicTabsDelegate) {
+        //        $scope.$on('$ionicView.enter', function(e) {
+        //        });
 
         $scope.chats = Chats.all();
         $scope.remove = function (chat) {
@@ -28,53 +28,53 @@ angular.module('starter.controllers', ['ionic', 'ngMaterial', 'ion-alpha-scroll'
     .controller('TeacherDetailCtrl', function ($scope, $stateParams, TeachersData) {
         $scope.teacherItem = TeachersData.get($stateParams.teacherId);
     })
-.controller('TeacherListCtrl', function ($scope, $state, TeachersData) {
-    $scope.data = TeachersData.all();
-    $scope.settings = {
-        enableFriends: true
-    };
-    $scope.goToDetail = function(index){
-        $state.go('tab.teacher-detail', {
-            teacherId: index
-        });
-    }
-
-    $scope.Alert = function(str){
-        alert(str);
-    }
-    $scope.$on('$ionicView.enter', function(e) {
-        $scope.filteredResults = $scope.data.results;
-    });
-
-    $scope.searchText = "";
-    $scope.searchQuery = [];
-
-    $scope.lastQuery = "";
-    $scope.queryMatches = function (query) {
-        if($scope.lastQuery != "")
-            if(query.length < $scope.lastQuery.length)
-                $scope.filteredResults = $scope.data.results;
-        var arr = [];
-        var tmp = [];
-        for (var i = 0; i < $scope.filteredResults.length; i++) {
-            var fname = $scope.filteredResults[i].firstName;
-            var lname = $scope.filteredResults[i].lastName;
-            if (fname.toLowerCase().indexOf(("" + query).toLowerCase()) == 0 ||
-                lname.toLowerCase().indexOf(("" + query).toLowerCase()) == 0){
-                arr.push($scope.filteredResults[i].name + "");
-                tmp.push($scope.filteredResults[i])
-            }
+    .controller('TeacherListCtrl', function ($scope, $state, TeachersData) {
+        $scope.data = TeachersData.all();
+        $scope.settings = {
+            enableFriends: true
+        };
+        $scope.goToDetail = function (index) {
+            $state.go('tab.teacher-detail', {
+                teacherId: index
+            });
         }
-        $scope.lastQuery = query;
-        $scope.filteredResults = tmp;
-//        console.log("Saagar: " + $scope.filteredResults.length)
-        $scope.searchQuery = arr;
-    };
-    $scope.showDivider = function (index) {
-        if (index == 0) return true;
-        if (index != $scope.data.results.length - 1 && index > 0)
-            if ($scope.data.results[index].lastName.charAt(0) != $scope.data.results[index - 1].lastName.charAt(0))
-                return true;
-    }
 
-});
+        $scope.Alert = function (str) {
+            alert(str);
+        }
+        $scope.$on('$ionicView.enter', function (e) {
+            $scope.filteredResults = $scope.data.results;
+        });
+
+        $scope.searchText = "";
+        $scope.searchQuery = [];
+
+        $scope.lastQuery = "";
+        $scope.queryMatches = function (query) {
+            if ($scope.lastQuery != "")
+                if (query.length < $scope.lastQuery.length)
+                    $scope.filteredResults = $scope.data.results;
+            var arr = [];
+            var tmp = [];
+            for (var i = 0; i < $scope.filteredResults.length; i++) {
+                var fname = $scope.filteredResults[i].firstName;
+                var lname = $scope.filteredResults[i].lastName;
+                if (fname.toLowerCase().indexOf(("" + query).toLowerCase()) == 0 ||
+                    lname.toLowerCase().indexOf(("" + query).toLowerCase()) == 0) {
+                    arr.push($scope.filteredResults[i].name + "");
+                    tmp.push($scope.filteredResults[i])
+                }
+            }
+            $scope.lastQuery = query;
+            $scope.filteredResults = tmp;
+            //        console.log("Saagar: " + $scope.filteredResults.length)
+            $scope.searchQuery = arr;
+        };
+        $scope.showDivider = function (index) {
+            if (index == 0) return true;
+            if (index != $scope.data.results.length - 1 && index > 0)
+                if ($scope.data.results[index].lastName.charAt(0) != $scope.data.results[index - 1].lastName.charAt(0))
+                    return true;
+        }
+
+    });
